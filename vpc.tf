cab9e8dfe2d6a8a63350ad41d57f10586375d55d
Lucas Omena <lucasomena5@gmail.com>
2022-05-28 10:13:54 -0300

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,16 @@
+//--------------------------------------------------------------------
+// Modules
+module "network" {
+  source  = "app.terraform.io/acn_desafio/network/google"
+  version = "5.0.0"
+
+  network_name = "gaurav-network"
+  project_id = var.project
+  subnets = [
+    {
+      subnet_name   = "gaurav-subnet"
+      subnet_ip     = "10.100.10.0/24"
+      subnet_region = var.region
+    }
+  ]
+}
